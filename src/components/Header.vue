84edc0ae0ba9dccf0234cfd1ce3e489e72a85597
scollo26 <giovanniscollo26@gmail.com>
2022-01-19 11:27:42 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+};
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+
+<style>
+
+</style>
